e13b77240f075d6ec927206d6b0f68ee3e1033d1
bigmonkey0913 <1657264154@qq.com>
2023-07-25 16:00:18 +0800

    20230725 7-commit

--- car2023-realtime/src/main/java/com/atguigu/app/dws/DwsEnergyChargeCycles.java
+++ car2023-realtime/src/main/java/com/atguigu/app/dws/DwsEnergyChargeCycles.java
@@ -1,11 +1,15 @@
 package com.atguigu.app.dws;
 
+import com.alibaba.fastjson.JSONObject;
 import com.atguigu.app.BaseApp;
 import com.atguigu.bean.EnergyChargeBean;
 import com.atguigu.common.Constant;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
+import org.apache.flink.util.Collector;
 
 public class DwsEnergyChargeCycles extends BaseApp {
 
@@ -28,7 +32,6 @@ public class DwsEnergyChargeCycles extends BaseApp {
 
         //1.解析成pojo
         parseToPoJo(stream);
-
         //2.开窗
 
         //3.聚合
@@ -36,15 +39,32 @@ public class DwsEnergyChargeCycles extends BaseApp {
 
     }
 
-    private void parseToPoJo(DataStreamSource<String> stream) {
+    private SingleOutputStreamOperator<EnergyChargeBean> parseToPoJo(DataStreamSource<String> stream) {
 
-        stream
-                .map(new MapFunction<String, EnergyChargeBean>() {
+       return stream
+                .map(new MapFunction<String, JSONObject>() {
                     @Override
-                    public EnergyChargeBean map(String value) throws Exception {
-                        return  JSON;
+                    public JSONObject map(String value) throws Exception {
+                        return JSONObject.parseObject(value);
+
+
+
                     }
                 })
+                .keyBy(bean -> bean.getString("vin"))
+                .process(new KeyedProcessFunction<String, JSONObject, EnergyChargeBean>() {
+                    @Override
+                    public void processElement(JSONObject obj,
+                                               Context ctx, 
+                                               Collector<EnergyChargeBean> out) throws Exception {
+                        String vin = obj.getString("vin");
+                        Long timestamp = obj.getLong("timestamp");
+
+                        out.collect(new EnergyChargeBean(vin,timestamp,0,"","",""));
+
+                        
+                    }
+                });
 
 
     }
